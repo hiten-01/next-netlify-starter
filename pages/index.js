2d3acbd0434b478ed5dff7deae19ab54b8783e12
hiten-01 <102731238+hiten-01@users.noreply.github.com>
2022-04-01 04:18:52 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,6 @@ export default function Home() {
       
         <main>
           <Header title="原神サポートへようこそ!" />
-          <p className="description">
-            このサイトは原神サポートの管理するサイトです。
-          </p>
           <p>
             御用の際は<a href="https://twitter.com/Hgrs71umnk" target="_blank" rel="noopener noreferrer">緋天</a>までご連絡ください。
           </p>
