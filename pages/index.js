8e1dad74127c42116e8d5ceb266edb4c9de71f01
hiten-01 <102731238+hiten-01@users.noreply.github.com>
2022-04-01 01:41:34 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <div background-image : url("/genshin-title.jpg");>
-          <Header title="原神サポートへようこそ!" />
-          <p className="description">
-            このサイトは原神サポートの管理するサイトです。
-          </p>
-          <p>
-            御用の際は<a href="https://twitter.com/Hgrs71umnk" target="_blank" rel="noopener noreferrer">緋天</a>までご連絡ください。
-          </p>
-          </div>
+        <Header title="原神サポートへようこそ!" />
+        <p className="description">
+          このサイトは原神サポートの管理するサイトです。
+        </p>
+        <p>
+          御用の際は<a href="https://twitter.com/Hgrs71umnk" target="_blank" rel="noopener noreferrer">緋天</a>までご連絡ください。
+        </p>
       </main>
 
       <Footer />
